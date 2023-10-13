6b7e917a2adb44eb2187ce8177dabb76893272fe
Fabio Macimo <fabio_maximo45@hotmail.com>
2023-10-13 10:51:19 -0300

    "Dockerfile deploy"

---
+++ src/test/java/br/com/fabiomacimo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiomacimo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
